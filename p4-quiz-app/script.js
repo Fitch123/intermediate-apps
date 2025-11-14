4b65c50eb72981df53e100a26fa65e194ae0ecbd
Cesar <cfitchf@wgu.edu>
2025-11-13 20:24:57 -0800

    created object array of questions, options and answers

---
+++ p4-quiz-app/script.js
@@ -0,0 +1,30 @@
+const nextBtn = document.getElementById("nextBtn");
+
+
+const questions = [
+    {
+        question: "What does console.log() do in JavaScript?",
+        options: ["Displays a message", "Creates a popup", "Changes HTML", "Stops code"],
+        answer: 0
+    },
+    {
+        question: "Which tag makes a hyperlink?",
+        options: ["<link>", "<href>", "<a>", "<p>"],
+        answer: 2
+    },
+    {
+        question: "Which CSS property changes text color?",
+        options: ["background-color", "color", "font-style", "text-color"],
+        answer: 1
+    }
+];
+
+
+
+
+
+
+
+
+
+nextBtn.addEventListener("click", () => console.log("yessurs"));
